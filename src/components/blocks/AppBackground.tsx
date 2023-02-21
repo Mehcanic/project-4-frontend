8cadeb3399cf823a4451c0d240b987a3aeedce06
Lukasz Bialy <bialy.lukasz0@gmail.com>
2023-02-21 22:59:09 +0000

    added axios; working on ListOfTask component - base style created for notActive version

--- src/components/blocks/AppBackground.tsx
+++ src/components/blocks/AppBackground.tsx
@@ -5,21 +5,21 @@ const HeaderBackground = styled.header`
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
-  width: 1440px;
+  max-width: 1440px;
   height: 300px;
   position: relative;
   z-index: -4;
 
   @media only screen and (max-width: 420px) {
     background-color: ${props => props.theme.background.bgImageMobile};
-    width: 100%;
+    max-width: 100%;
     height: 200px;
   }
 `
 
 const BackgroundWrapper = styled.div`
-  background-color: ${props => props.theme.colors.secondary};
-  width: 100%;
+  background-color: ${props => props.theme.colors.primary};
+  max-width: 100%;
   height: 100vh;
   position: relative;
   z-index: -5;
--- src/components/blocks/ListOfTasks.tsx
+++ src/components/blocks/ListOfTasks.tsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react'
+
 import styled, { DefaultTheme, ThemeProvider } from "styled-components"
 import { lightTheme, darkTheme } from '../../styles/theme'
 
@@ -5,20 +7,115 @@ type ListProps = {
   theme: DefaultTheme;
 }
 
+type Task = {
+  id: number;
+  list_of_tasks_id: number;
+  name: string;
+  description: string;
+  status: string;
+  created_at: string;
+  updated_at: string;
+  date: string;
+  time: string;
+}
+
 const List = styled.div`
-  background-color: ${props => props.theme.colors.primary};
+  background-color: ${props => props.theme.colors.secondary};
+  color: ${props => props.theme.colors.accentLight};
   width: 540px;
   height: auto;
+  margin: 0 auto;
+  border-radius: 5px;
 
+` 
+const ListItem = styled.div`
+  display: flex;  
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  align-content: center;
+  width: 492px;
+  min-height: 40px;
+  border-bottom: 1px solid ${props => props.theme.colors.primary};
+
+  .item-container {
+    margin: 12px 0;
+
+      .cicrle {
+        border: 1px solid ${props => props.theme.colors.primary};
+        border-radius: 50%;
+        width: 24px;
+        height: 24px;
+        margin-left: 24px;
+      }
+
+      p {
+        overflow-wrap: break-word;
+        word-wrap: break-word;
+        hyphens: auto;
+        max-width: 400px;
+        margin: 0 24px;
+      }
+
+      .remove {
+        border-radius: 50%;
+        width: 24px;
+        height: 24px;
+        margin-right: 24px;
+      }
+}
 `
 
 const ListOfTasks = ({ theme }: ListProps) => {
+// localhost:5000/api/users/tasks
 
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [error, setError] = useState<string | null>(null)
+
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:5000/api/users/tasks')
+      const data = await response.json()
+      console.log(data)
+      setTasks(data)
+    } catch (error: any) {
+      setError(error)
+    }
+  }
+
+  if (!tasks) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>
+  }
+
+  useEffect(() => {
+    fetchTasks()
+  }, [])
 
   return (
     <>
       <ThemeProvider theme={theme}>
-        <List />
+        <List>
+          {tasks.sort((a, b) => a.id - b.id).map(item => {
+            return (
+              <ListItem key={item.id}>
+                <div className='item-container'>
+                  <div className='cicrle'></div>
+                </div>
+                <div className='item-container'>
+                  <p>{item.name}</p>
+                </div>
+                <div className='item-container'>
+                  <div className='remove'></div>
+                </div>
+              </ListItem>
+            )
+          })
+          }
+        </List>
       </ThemeProvider>
     </>
   )
